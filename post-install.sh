f4b5f67314344d42c41ed791a235bb029ce356e2
Rubysco <rubysc0@protonmail.com>
2020-08-12 18:25:26 +0200

    Add mkinitcpio to post install script

--- post-install.sh
+++ post-install.sh
@@ -4,6 +4,7 @@ cp /run/archiso/bootmnt/arch/boot/x86_64/vmlinuz /mnt/boot/vmlinuz-linux
 sed -i 's/volatile/persistent/g' /mnt/etc/systemd/journald.conf
 
 arch-chroot /mnt /bin/bash -c "systemctl enable dhcpcd"
+arch-chroot /mnt /bin/bash -c "mkinitcpio -p linux"
 arch-chroot /mnt /bin/bash -c "grub-install /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg"
 
 genfstab -U /mnt >> /mnt/etc/fstab
